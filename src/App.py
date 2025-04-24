2106f7757f420ceae6a46c4bedd8660b0e95ab30
Hiroshi-Yamashita-Santos <hiroshiysantos@gmail.com>
2025-04-24 20:54:07 -0300

    refactor: mudança de port para 80

--- src/App.py
+++ src/App.py
@@ -222,6 +222,6 @@ def grafico_quarto():
 #----------------- Inicia o servidor Flask ---------------
 #Roda a aplicação localmente com debug=True (útil durante o desenvolvimento).
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
 
 
